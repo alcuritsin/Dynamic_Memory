3123d7c123624d96e91fa58a6f002a573cb8421c
alcuritsin <al.curitsin@gmail.com>
2021-01-27 20:11:46 +0500

    HW_DynamicMemory --done

---
+++ CW_Split/main.cpp
@@ -0,0 +1,74 @@
+﻿#include<iostream>
+#include<ctime>
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int n = 10;
+	int arr[n];
+
+	std::cout << time(NULL) << std::endl;
+
+	srand(time(NULL));
+	FillRand(arr, n);
+	Print(arr, n);
+
+	int even_count = 0;
+	int odd_count = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			even_count++;
+		}
+		else
+		{
+			odd_count++;
+		}
+	}
+
+	int* even_values = new int [even_count] {};
+	int* odd_values = new int [odd_count] {};
+
+	for (int i = 0, i_even = 0, i_odd = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			even_values[i_even++] = arr[i];
+		}
+		else
+		{
+			odd_values[i_odd++] = arr[i];
+		}
+	}
+
+	std::cout << "Массив ЧЕТНЫХ значений:\n";
+	Print(even_values, even_count);
+
+	std::cout << "Массив НЕ ЧЕТНЫХ значений:\n";
+	Print(odd_values, odd_count);
+
+	delete[]even_values;
+	delete[]odd_values;
+
+	std::cout << "Программа завершена.\n";
+}
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << arr[i] << "  ";
+	}
+	std::cout << std::endl << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,70 @@
+﻿//TODO:
+/*
+В проект DynamicMemory добавить алгоритм добавления элемента в конец массива.
+*/
+
+#include<iostream>
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	std::cout << "Введите размер массива: "; std::cin >> n;
+	int* arr = new int[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
+	
+	int value;
+	std::cout << "Введите размер значение: "; std::cin >> value;
+	
+	int* brr = new int[++n];
+	for (int i = 0; i < n - 1; i++)
+	{
+		brr[i] = arr[i];
+	}
+
+	brr[n - 1] = value;
+	delete[] arr;
+
+	Print(brr, n);
+
+	delete[] brr;
+
+	std::cout << "Программа завершена.";
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand()%100;
+	}
+}
+
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << arr[i] << "  ";
+	}
+	std::cout << std::endl << std::endl;
+}
+
+//Исполнитель
+/*
+-----------------------------------------------------
+|                                                   |
+|   "Компьютерная академия ШАГ"                     |
+|   Курс: БД011                                     |
+|   Предмет: Основы програмvирования на языке C++   |
+|                                                   |
+|   Исполнитель: Курицын Алексей                    |
+|   Преподаватель: Ковтун Олег                      |
+|                                                   |
+|   Екатеринбург - 2021                             |
+|                                                   |
+-----------------------------------------------------*/
